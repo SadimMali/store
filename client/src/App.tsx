aee7c0b1d42facb1311b30f83f98a39d24427fe9
Sadim Mali <sadimmalakar77@gmail.com>
2024-12-28 21:42:43 +0545

    feat(client): Implement routing with Layout and Homepage components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Homepage from "./pages/Homepage";
+import Layout from "./components/page/Layout";
+
 export default function App() {
-  return (
-    <div className="text-red-400">App</div>
-  )
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
