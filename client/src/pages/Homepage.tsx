4919b2253ff4506ffbf4e155484b5f6378411291
Sadim Mali <sadimmalakar77@gmail.com>
2024-12-28 21:39:14 +0545

    feat: Create Homepage with Hero section

---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/home/Hero";
+
+const Homepage = () => {
+  return (
+    <>
+      {/* hero section */}
+      <Hero />
+    </>
+  );
+};
+
+export default Homepage;
