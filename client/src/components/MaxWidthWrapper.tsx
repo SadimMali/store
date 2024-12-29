6d1254bb2523ef19c25a90a7a07d76947ce7616c
Sadim Mali <sadimmalakar77@gmail.com>
2024-12-29 21:40:21 +0545

    feat: Add MaxWidthWrapper component for responsive layout management

---
+++ client/src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+
+type MaxWidthProps = {
+  className: string;
+  children: React.ReactNode;
+};
+
+const MaxWidthWrapper = ({ className, children }: MaxWidthProps) => {
+  return (
+    <div className={cn("h-full w-full px-2.5 md:px-10 lg:px-20", className)}>
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
