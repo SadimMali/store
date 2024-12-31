2948c61fb682c91490e0dc6c2cad2f35289925ab
Sadim Mali <sadimmalakar77@gmail.com>
2024-12-31 21:21:15 +0545

    feat: Add authentication routes with AuthLayout and related pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Homepage from "./pages/Homepage";
 import Layout from "./components/page/Layout";
+import AuthLayout from "./components/page/AuthLayout";
+import Loginpage from "./pages/auth/Loginpage";
+import SignUpPage from "./pages/auth/SignUpPage";
+import VerifyPage from "./pages/auth/VerifyPage";
 
 export default function App() {
   const router = createBrowserRouter([
@@ -10,6 +14,23 @@ export default function App() {
         {
           path: "/",
           element: <Homepage />,
+        },
+      ],
+    },
+    {
+      element: <AuthLayout />,
+      children: [
+        {
+          path: "/login",
+          element: <Loginpage />,
+        },
+        {
+          path: "/sign-up",
+          element: <SignUpPage />,
+        },
+        {
+          path: "/verify/:id",
+          element: <VerifyPage />,
         },
       ],
     },
