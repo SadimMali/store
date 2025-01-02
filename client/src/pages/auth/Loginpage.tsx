f339bc3986a38d1119294c3b65729717e729c2f3
Sadim Mali <sadimmalakar77@gmail.com>
2025-01-02 21:57:45 +0545

    feat: Add Loginpage component with form for user authentication

---
+++ client/src/pages/auth/Loginpage.tsx
@@ -0,0 +1,81 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { loginSchema } from "@/schemas/loginSchema";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/types/Login.type";
+
+const Loginpage = () => {
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: LoginSchema) => {
+    console.log(data);
+  };
+  return (
+    <div className="">
+      <h1 className="text-2xl font-semibold py-4">
+        Enter your email to join us or sign in{" "}
+      </h1>
+
+      {/* form */}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="identifier"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username / Email </FormLabel>
+                <FormControl>
+                  <Input placeholder="username/email " {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+
+      <p className="text-md text-center mt-4 ">
+        <span className="font-medium"> Not a member yet?</span>
+        <Link
+          to="/sign-up"
+          className=" ml-2 font-bold text-blue-600 hover:text-blue-800"
+        >
+          Sign up
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default Loginpage;
