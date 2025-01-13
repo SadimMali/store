060a58d6743b0c696d7f214a79736892e035b013
Sadim Mali <sadimmalakar77@gmail.com>
2025-01-13 23:37:46 +0545

    feat: Define types for ImageCarousel and ProductCategories

---
+++ client/src/types/Carousel.type.ts
@@ -0,0 +1,10 @@
+export type ImageCarousel = {
+    imgSrc: string;
+    heading: string;
+    description: string;
+  };
+
+  export type ProductCategories = {
+    id: number;
+    name: string;
+  }
